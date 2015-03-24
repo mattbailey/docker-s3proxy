4405a1119fcdcb2c8f82eb1d368469f62aea6add
Matt Bailey <m@mdb.io>
2015-03-24 15:49:24 -0400

    single bucket

--- start.sh
+++ start.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-# expect comma delim'd buckets
-BUCKETS=`echo $AWS_BUCKETS | sed -e 's/,/ /g'`
-
 # Generate whitelist
-WHITELIST=`echo $NGINX_WHITELIST |sed -e 's/,/ /g'`
+#WHITELIST=`echo $NGINX_WHITELIST |sed -e 's/,/ /g'`
 
-for network in $WHITELIST; do
-  echo "allow ${network};" >> /etc/nginx/whitelist.conf
-done
+#for network in $WHITELIST; do
+#  echo "allow ${network};" >> /etc/nginx/whitelist.conf
+#done
 
 # Generate basic auth file(s)
-echo 'auth_basic "access denied"' > /etc/nginx/auth.conf
-echo 'auth_basic_user_file "/etc/nginx/htpasswd"' >> /etc/nginx/auth.conf
+#echo 'auth_basic "access denied"' > /etc/nginx/auth.conf
+#echo 'auth_basic_user_file "/etc/nginx/htpasswd"' >> /etc/nginx/auth.conf
 
 # So that SIGTERM from docker propagates to nginx
 trap "echo \"Sending SIGTERM to nginx\"; killall -s SIGTERM nginx" SIGTERM
@@ -22,10 +19,8 @@ mkdir -p /buckets
 nginx -g 'daemon off;' &
 
 while [ 0 ]; do
-  for bucket in $BUCKETS; do
-    mkdir -p /buckets/${bucket}
-    echo "Syncing bucket ${bucket}"
-    s3cmd --delete-after-fetch --access_key=${AWS_ACCESS_KEY_ID} --secret_key=${AWS_ACCESS_SECRET_KEY} --no-progress sync s3://${bucket} /buckets/${bucket}
-  done
+  mkdir -p /buckets/${AWS_BUCKET}
+  echo "Syncing bucket ${AWS_BUCKET}"
+  s3cmd --delete-after-fetch --access_key=${AWS_ACCESS_KEY_ID} --secret_key=${AWS_ACCESS_SECRET_KEY} --no-progress sync s3://${AWS_BUCKET} /buckets
   sleep ${INTERVAL}
 done
